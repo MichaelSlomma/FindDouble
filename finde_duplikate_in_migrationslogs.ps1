874300d04d012715395cdcf82492166b890fe5f7
Runkel-DE <81742894+Runkel-DE@users.noreply.github.com>
2021-04-09 09:03:42 +0200

    Add files via upload
    completely rewrote the code to provide multiple file import and way much better performance by removing any progress related output
    only a time mesurement is now implemented
    
    the tool is now at least 90% faster

---
+++ finde_duplikate_in_migrationslogs.ps1
@@ -0,0 +1,65 @@
+﻿#
+# Findet doppelte Einträge in Archivmigrationslogs
+# 
+# (c) 2021 H&H Datenverarbeitungs- und Beratungsgesellschaft mbH
+# 
+# Autor: Michael Slommma
+#
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.Filter = "Logfiles|archivmigration*.log"
+$OpenFileDialog.Title = "Datei(en) auswählen"
+$OpenFileDialog.Multiselect = $true
+$OpenFileDialog.ShowDialog() | Out-Null
+$LogFiles = $OpenFileDialog.FileNames
+
+[ARRAY]$dummyarray=""
+[System.Collections.ArrayList]$analyse = $dummyarray
+[System.Collections.ArrayList]$output = $dummyarray
+
+$timestamp = Get-Date -format "yyyyMMdd_HHmmss"
+$duplicateslog = ".\Duplikate_"+$timestamp+".log"
+$starttime = Get-Date
+
+<#
+Jetzt müssen wir jeden einzelnen Log-Eintrag erstmal um die ersten 53 Zeichen
+kürzen. Das ist der Timestamp der jeweiligen Aktion. Der ändert sich ja permantent
+und würde damit eine Duplikatserkennung unmöglich machen.
+#>
+Write-Host "Bereite Dateien vor..."
+foreach ($file in $logfiles)
+    {
+        $content = get-content $file
+        foreach ($row in $content)
+            {
+                $dummy = $analyse.Add( ($row.Remove(0,53)).trim() )
+            }
+    }
+
+
+<#
+Über ein HashTable suchen wir nun die Duplikate, indem wir für jeden gefundenen Hash den Wert in der Tabelle um 1 erhöhen.
+Werte >1 verweisen auf Duplikate
+Zeilen die mit "Start Migration" beginnen lassen wir dabei unberücksichtigt, da sie das Ergebnis verfälschen würden.
+#>
+Write-Host "Suche Duplikate..."
+$ht = @{}
+$analyse | foreach {$ht["$_"] += 1}
+$ht.keys | where {$ht["$_"] -gt 1 -and $_ -ne "Start Migration:"} | foreach { $dummy = $output.Add( $_ ) ; $duplicates++ }
+
+#
+# Das Ergebnis der Analyse schreiben wir dann in eine Log-Datei.
+#
+$output | Out-File -FilePath $duplicateslog
+"`n`rInsgesamt $duplicates Duplikate gefunden" >> $duplicateslog
+
+#
+# Auswertung und Abschluss
+#
+$endtime = Get-Date
+$timedifference = $endtime - $starttime
+$runningtime = [math]::Round($timedifference.TotalSeconds,3)
+Write-Host "`n`rInsgesamt $duplicates Duplikate gefunden"
+Write-Host "`n`rVerarbeitungszeit: $runningtime Sekunden"
+Read-Host “`n`r`n`rZum Beenden <Enter> drücken”
